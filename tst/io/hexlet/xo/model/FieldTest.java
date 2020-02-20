f63fe032790472f84a52a5d91d52d7c539c356db
Sanzhar Suleimenov <sanzhar.suleimenov@icloud.com>
2020-02-20 19:51:52 +0600

    FieldTest - field checks have been added, and throws exceptions were improved.

--- tst/io/hexlet/xo/model/FieldTest.java
+++ tst/io/hexlet/xo/model/FieldTest.java
@@ -17,7 +17,7 @@ public class FieldTest {
     }
 
     @Test
-    public void setFigure() throws InvalidPointException, AlreadyOccupiedException {
+    public void setFigure() throws Exception {
         final Field field = new Field();
         final Point inputPoint = new Point(0,0);
         final Figure inputFigure = Figure.O;
@@ -26,6 +26,95 @@ public class FieldTest {
         final Figure actualFigure = field.getFigure(inputPoint);
 
         assertEquals(inputFigure, actualFigure);
-
     }
+
+    @Test
+    public void testGetFigureWhenFigureIsNotSet() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,0);
+        final Figure actualFigure = field.getFigure(inputPoint);
+
+        assertNull(actualFigure);
+    }
+
+    @Test
+    public void testGetFigureWhenXIsLessThanZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1,0);
+        /*
+            Мы не можем взять фигуру из точки с отрицательной координаты и поэтому тест не должен дойти до 49 строки и перейти в catch.
+         */
+        try {
+            final Figure actualFigure = field.getFigure(inputPoint);
+            fail(); // JUnit завалит тест если исполнение программы дойдет до этой строки
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    public void testGetFigureWhenYIsLessThanZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,-1);
+        /*
+            Мы не можем взять фигуру из точки с отрицательной координаты и поэтому тест не должен дойти до 49 строки и перейти в catch.
+         */
+        try {
+            final Figure actualFigure = field.getFigure(inputPoint);
+            fail(); // JUnit завалит тест если исполнение программы дойдет до этой строки
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    public void testGetFigureWhenXYIsLessThanZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(-1,-1);
+        /*
+            Мы не можем взять фигуру из точки с отрицательной координаты и поэтому тест не должен дойти до 49 строки и перейти в catch.
+         */
+        try {
+            final Figure actualFigure = field.getFigure(inputPoint);
+            fail(); // JUnit завалит тест если исполнение программы дойдет до этой строки
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    public void testGetFigureWhenXIsMoreThanZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(field.getSize() + 1,0);
+        /*
+            Мы не можем взять фигуру из точки с отрицательной координаты и поэтому тест не должен дойти до 49 строки и перейти в catch.
+         */
+        try {
+            final Figure actualFigure = field.getFigure(inputPoint);
+            fail(); // JUnit завалит тест если исполнение программы дойдет до этой строки
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    public void testGetFigureWhenYIsMoreThanZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(0,field.getSize() + 1);
+        /*
+            Мы не можем взять фигуру из точки с отрицательной координаты и поэтому тест не должен дойти до 49 строки и перейти в catch.
+         */
+        try {
+            final Figure actualFigure = field.getFigure(inputPoint);
+            fail(); // JUnit завалит тест если исполнение программы дойдет до этой строки
+        } catch (final InvalidPointException e) {}
+    }
+
+    @Test
+    public void testGetFigureWhenXYIsMoreThanZero() throws Exception {
+        final Field field = new Field();
+        final Point inputPoint = new Point(field.getSize() + 1,field.getSize() + 1);
+        /*
+            Мы не можем взять фигуру из точки с отрицательной координаты и поэтому тест не должен дойти до 49 строки и перейти в catch.
+         */
+        try {
+            final Figure actualFigure = field.getFigure(inputPoint);
+            fail(); // JUnit завалит тест если исполнение программы дойдет до этой строки
+        } catch (final InvalidPointException e) {}
+    }
+
+
+
 }
