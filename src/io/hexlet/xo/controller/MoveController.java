efd384df74fc5bb08f94a0a5716c5c93040e571e
Sanzhar Suleimenov <sanzhar.suleimenov@icloud.com>
2020-02-22 20:46:46 +0600

    CurrentMoveController - class update. Checking already occupied exception, that were moved from Field class.

--- src/io/hexlet/xo/controller/MoveController.java
+++ src/io/hexlet/xo/controller/MoveController.java
@@ -2,13 +2,25 @@ package io.hexlet.xo.controller;
 
 import io.hexlet.xo.model.Field;
 import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.exceptions.AlreadyOccupiedException;
+import io.hexlet.xo.model.exceptions.InvalidPointException;
 
 import java.awt.*;
 
 public class MoveController {
 
-    public void applyFigure(final Field field, final Figure figure, final Point point) throws Exception {
+    public void applyFigure(final Field field,
+                            final Figure figure,
+                            final Point point) throws   AlreadyOccupiedException,
+                                                        InvalidPointException {
+
+        if (field.getFigure(point) != null) {
+            throw new AlreadyOccupiedException();
+        }
         field.setFigure(point, figure);
+
     }
+
+
 
 }
