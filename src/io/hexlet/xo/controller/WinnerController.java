2d74427d156438935063bbe6c9802d16b5b61884
Sanzhar Suleimenov <sanzhar.suleimenov@icloud.com>
2020-02-22 20:23:36 +0600

    WinnerController - class created.

---
+++ src/io/hexlet/xo/controller/WinnerController.java
@@ -0,0 +1,12 @@
+package io.hexlet.xo.controller;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+
+public class WinnerController {
+
+    public Figure getWinner(final Field field) {
+        return Figure.X;
+    }
+
+}
